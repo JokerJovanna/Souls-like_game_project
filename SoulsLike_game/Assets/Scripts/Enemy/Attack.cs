6507907d0259bd127b40a85708d6b435cc2ede20
JokerJovanna <artemkuricev956@gmail.com>
2026-04-12 00:25:02 +0500

    Добавить компонент атаки и базовую ближнюю атаку

---
+++ SoulsLike_game/Assets/Scripts/Enemy/Attack.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public abstract class Attack : MonoBehaviour
+{
+    public abstract float AttackDistance { get; }
+    public abstract void Perform(GameObject attacker, GameObject target);
+
+}
---
+++ SoulsLike_game/Assets/Scripts/Enemy/Skeleton/Attack_types/SingleMeleeAttack.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SingleMeleeAttack : Attack
+{
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private bool canBeBlocked = true;
+    [SerializeField] private float attackDistance = 1.5f;
+
+    public override float AttackDistance => attackDistance;
+
+    public override void Perform(GameObject attacker, GameObject target)
+    {
+        if (target == null) return;
+        if (Vector2.Distance(target.transform.position, attacker.transform.position) > attackDistance) return;
+
+        var healthComponent = target.GetComponent<HealthComponent>();
+
+        if (healthComponent == null) return;
+
+        var attack = new AttackData(damage, attacker, canBeBlocked);
+        healthComponent.TakeDamage(attack);
+    }
+}
