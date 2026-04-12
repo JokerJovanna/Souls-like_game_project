d693ba3cd528ceb660a657bebe6c123dd2d0090e
yeey71 <egzakirov@yandex.ru>
2026-04-12 19:27:39 +0500

    Стамина

--- SoulsLike_game/Assets/Scripts/Player.cs
+++ SoulsLike_game/Assets/Scripts/Player.cs
@@ -1,54 +1,125 @@
 using System;
 using UnityEngine;
 
-public class Player : MonoBehaviour, IPlayer
+public class Player : MonoBehaviour
 {
     private readonly float speed = 5.0f;
+    private Rigidbody2D rb;
+
+
     private readonly float jumpForce = 10.0f;
+    private readonly int maxJumps = 2;
+    public float jumpCooldown = 0.2f;
     private int remainingJumps;
+    private float nextJumpTime;
 
     public Transform groundCheck;
     public LayerMask groundLayer;
     public float groundCheckRadius = 0.1f;
-
-    private Rigidbody2D rb;
     private bool isGrounded;
+    private bool wasGrounded;
+
+    public float dodgeDistance = 3f;
+    public float dodgeDuration = 0.2f;
+    public float dodgeCooldown = 1f;
+    private bool isDodging = false;
+    private float dodgeEndTime = 0f;
+    private float nextDodgeTime = 0f;
+    private Vector2 dodgeDirection;
+
+    public float maxStamina = 100f;
+    private float currentStamina;
+    public float staminaRegenRate = 20f;
+    public float staminaCostDodge = 30f;
+    public float staminaCostJump = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        remainingJumps = maxJumps;
+        currentStamina = maxStamina;
     }
 
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
-        if (isGrounded)
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 
+            groundCheckRadius, groundLayer);
+        
+        if (isGrounded && !wasGrounded)
         {
-            remainingJumps = 2;
+            remainingJumps = maxJumps;
+        }
+        wasGrounded = isGrounded;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDodging &&
+            Time.time >= nextDodgeTime)
+        {
+            Dodge();
         }
 
-        if (Input.GetButtonDown("Jump") && (isGrounded || remainingJumps < 2) && remainingJumps > 0)
+        if (isDodging && Time.time >= dodgeEndTime)
         {
-            Debug.Log(remainingJumps);
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            remainingJumps -= 1;
+            isDodging = false;
+            rb.gravityScale = 1f;
         }
 
+
+        if (Input.GetKeyDown(KeyCode.Space) && 
+            remainingJumps > 0 && Time.time >= nextJumpTime)
+        {
+            if (currentStamina >= staminaCostJump)
+            {
+                currentStamina -= staminaCostJump;
+                OnStaminaChanged?.Invoke(currentStamina, maxStamina);
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                remainingJumps--;
+                nextJumpTime = Time.time + jumpCooldown;
+            }
+        }
+
+        if (currentStamina < maxStamina)
+        {
+            currentStamina += staminaRegenRate * Time.deltaTime;
+            if (currentStamina > maxStamina) currentStamina = maxStamina;
+            OnStaminaChanged?.Invoke(currentStamina, maxStamina);
+        }
+        Debug.Log(currentStamina);
     }
 
     void FixedUpdate()
     {
-        float moveX = Input.GetAxis("Horizontal");
+        if (isDodging) return;
 
-        Vector2 movement = new Vector2(moveX * speed * Time.fixedDeltaTime, rb.linearVelocity.y);
-        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+        float moveX = 0f;
+        if (Input.GetKey(KeyCode.A)) moveX = -1f;
+        if (Input.GetKey(KeyCode.D)) moveX = 1f;
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
     }
 
+    public void Dodge()
+    {
+        if (currentStamina < staminaCostDodge) return;
 
-    public float CurrentStamina => throw new System.NotImplementedException();
-    public float MaxStamina => throw new System.NotImplementedException();
+        currentStamina -= staminaCostDodge;
+        OnStaminaChanged?.Invoke(currentStamina, maxStamina);
+
+        float moveX = 0f;
+        if (Input.GetKey(KeyCode.A)) moveX = -1f;
+        if (Input.GetKey(KeyCode.D)) moveX = 1f;
+        if (moveX == 0) moveX = 1f;
+
+        dodgeDirection = new Vector2(moveX, 0).normalized;
+
+        isDodging = true;
+        dodgeEndTime = Time.time + dodgeDuration;
+        nextDodgeTime = Time.time + dodgeCooldown;
+
+        rb.gravityScale = 0f;
+        float dodgeSpeed = dodgeDistance / dodgeDuration;
+        rb.velocity = dodgeDirection * dodgeSpeed;
+    }
+
     public float Damage => throw new System.NotImplementedException();
     public float CurrentHealth => throw new System.NotImplementedException();
     public float MaxHealth => throw new System.NotImplementedException();
@@ -66,11 +137,6 @@ public class Player : MonoBehaviour, IPlayer
     }
 
     public void Block()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void Dodge()
     {
         throw new System.NotImplementedException();
     }
